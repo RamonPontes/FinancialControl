c295b0ad4982fb87fb1f0096bf9b2872250895ef
RamonPontes <rgdpontes@yahoo.com>
2024-12-29 21:40:36 -0300

    add getAllFinances router

--- src/main/java/com/ramon/FinancialControl/controller/FinanceController.java
+++ src/main/java/com/ramon/FinancialControl/controller/FinanceController.java
@@ -1,15 +1,27 @@
 package com.ramon.FinancialControl.controller;
 
+import com.ramon.FinancialControl.service.FinanceServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class FinanceController {
+
+    @Autowired
+    private FinanceServices financeServices;
+
     @GetMapping("/")
     public ResponseEntity<?> helloWorld() {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAllFinances() {
+        return ResponseEntity.ok(financeServices.getAllFinances());
     }
 }
