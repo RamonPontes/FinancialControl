a1047cb64180cef666ed267fc9efca95775c3e47
RamonPontes <rgdpontes@yahoo.com>
2024-12-29 21:39:11 -0300

    edit finance model

--- src/main/java/com/ramon/FinancialControl/models/Finance.java
+++ src/main/java/com/ramon/FinancialControl/models/Finance.java
@@ -1,6 +1,14 @@
 package com.ramon.FinancialControl.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Finance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
